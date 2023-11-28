593568c5d55036cf6afef7e0f836d1062869a165
Billy Huang <10646003@ntub.edu.tw>
2023-11-28 10:40:59 +0800

    feat(katex-editor): toolbar add latex button

--- src/components/KatexEditor/index.tsx
+++ src/components/KatexEditor/index.tsx
@@ -1,9 +1,12 @@
 import { useState } from 'react';
 
+import Image from 'next/image';
+
+import LatexIcon from '@/public/images/markdown-editor/latex.svg';
+import * as commands from '@uiw/react-md-editor/commands';
 import katex from 'katex';
 import dynamic from 'next/dynamic';
 import { getCodeString } from 'rehype-rewrite';
-import rehypeSanitize from 'rehype-sanitize';
 
 import '@uiw/react-markdown-preview/markdown.css';
 import '@uiw/react-md-editor/markdown-editor.css';
@@ -14,22 +17,121 @@ const MDEditor = dynamic(
   { ssr: false },
 );
 
+const commandLatex: commands.ICommand = {
+  name: 'title3',
+  keyCommand: 'title3',
+  buttonProps: { 'aria-label': 'Insert title3' },
+  icon: <Image src={LatexIcon} alt='latex' width={16} height={16} />,
+  execute: (state: any, api: any) => {
+    let modifyText = `\
+\`\`\`latex
+${state.selectedText}
+\`\`\``;
+    if (!state.selectedText) {
+      modifyText = `\
+\`\`\`latex
+\`\`\``;
+    }
+    api.replaceSelection(modifyText);
+  },
+};
+
+const customCommands = [
+  commands.bold,
+  commands.italic,
+  commands.strikethrough,
+  commands.hr,
+  commands.group(
+    [
+      commands.title1,
+      commands.title2,
+      commands.title3,
+      commands.title4,
+      commands.title5,
+      commands.title6,
+    ],
+    {
+      name: 'title',
+      groupName: 'title',
+      buttonProps: { 'aria-label': 'Insert title' },
+    },
+  ),
+  commands.divider,
+
+  commands.link,
+  commands.quote,
+  commands.code,
+  commands.codeBlock,
+  commands.comment,
+  commandLatex,
+  commands.image,
+  commands.table,
+  commands.divider,
+
+  commands.unorderedListCommand,
+  commands.orderedListCommand,
+  commands.checkedListCommand,
+  commands.divider,
+
+  commands.help,
+];
+
 const KatexEditor = () => {
   const [textExpression, setTextExpression] = useState<string | undefined>(
     `\
-\`\`\`KaTex
-c = \\pm\\sqrt{a^2 + b^2}
+# Header
+
+Your content here
+
+**Bold**
+
+_Italic_
+
+~~line cross~~
+
+x~2~
+
+x^2^
+
+> blockauote
+> {.is-info}
+
+- item
+- item
+- item
+
+1. item
+2. item
+3. item
+
+\`console.log('Hello. world!');\`
+
 \`\`\`
-    `,
+console.log('Hello. world!');
+\`\`\`
+
+\`\`\`javascript
+console.log('Hello. world!');
+\`\`\`
+
+---
+
+\`\`\`latex
+c = \\pm\\sqrt{a^2 + b^2}
+\`\`\``,
   );
 
   return (
-    <article data-color-mode='dark' style={{ width: '100%' }}>
+    <article style={{ width: '100%' }}>
       <MDEditor
+        data-color-mode='dark'
         value={textExpression}
         onChange={setTextExpression}
+        commands={customCommands}
+        height='100%'
+        visibleDragbar={false}
         previewOptions={{
-          rehypePlugins: [[rehypeSanitize]],
+          // rehypePlugins: [[rehypeSanitize]],
           components: {
             code: ({
               inline,
@@ -62,7 +164,7 @@ c = \\pm\\sqrt{a^2 + b^2}
               if (
                 typeof code === 'string' &&
                 typeof className === 'string' &&
-                /^language-katex/.test(className.toLocaleLowerCase())
+                /^language-latex/.test(className.toLocaleLowerCase())
               ) {
                 const html = katex.renderToString(code, {
                   throwOnError: false,
