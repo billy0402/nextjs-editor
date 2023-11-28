ca7f141944669ccdaf719bb4ff037ac9f51339af
Billy Huang <10646003@ntub.edu.tw>
2023-11-28 12:03:27 +0800

    ci(github): add workflows

--- next.config.js
+++ next.config.js
@@ -3,6 +3,11 @@ const removeImports = require('next-remove-imports')();
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  output: 'export',
+  basePath: '/nextjs-editor',
+  images: {
+    unoptimized: true,
+  },
 };
 
 module.exports = removeImports(nextConfig);
