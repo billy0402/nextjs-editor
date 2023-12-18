8d57b23baa932b0221e85e2a0c764953026f1cbc
Billy Huang <10646003@ntub.edu.tw>
2023-12-18 17:40:53 +0800

    feat(tabs): add draggable

--- src/components/Tabs/Tabs.tsx
+++ src/components/Tabs/Tabs.tsx
@@ -1,4 +1,6 @@
+import { arrayMove } from '@/helpers/array';
 import type { File } from '@/models/archive';
+import { useRef, useState } from 'react';
 import { IoIosCloseCircleOutline } from 'react-icons/io';
 
 type Props = {
@@ -8,13 +10,41 @@ type Props = {
 };
 
 const Tabs = ({ files, setFiles, setSelectedFile }: Props) => {
+  const listRef = useRef<HTMLUListElement>(null);
+
+  const [startDrag, setStartDrag] = useState<string>();
+
   return (
-    <ul className='tabs'>
+    <ul ref={listRef} className='tabs'>
       {files.map((file) => (
         <li
           key={file.id}
           className='tabs__tab'
           onClick={() => setSelectedFile(file)}
+          draggable
+          onDragStart={(e) => {
+            setStartDrag((e.target as HTMLElement).innerText);
+          }}
+          onDragOver={(e) => {
+            const endDrag = (e.target as HTMLElement).innerText;
+
+            if (!startDrag || !endDrag || startDrag === endDrag) return;
+
+            const copyFiles = [...files];
+            const oldIndex = files.findIndex(
+              (_file) => _file.fullPath === startDrag,
+            );
+            const newIndex = files.findIndex(
+              (_file) => _file.fullPath === endDrag,
+            );
+            const newFiles = arrayMove<typeof copyFiles>(
+              copyFiles,
+              oldIndex,
+              newIndex,
+            );
+            setFiles(newFiles);
+            setSelectedFile(newFiles[newIndex]);
+          }}
         >
           {file.fullPath}
           <a
---
+++ src/helpers/array.ts
@@ -0,0 +1,20 @@
+export function arrayMove<T extends unknown[]>(
+  arr: T,
+  oldIndex: number,
+  newIndex: number,
+): typeof arr {
+  while (oldIndex < 0) {
+    oldIndex += arr.length;
+  }
+  while (newIndex < 0) {
+    newIndex += arr.length;
+  }
+  if (newIndex >= arr.length) {
+    var k = newIndex - arr.length + 1;
+    while (k--) {
+      arr.push(undefined);
+    }
+  }
+  arr.splice(newIndex, 0, arr.splice(oldIndex, 1)[0]);
+  return arr;
+}
